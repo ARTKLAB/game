3f3f24de3ad8dedbb217bd711f0ef4d2cbebdb81
JBahc <mailofpj@gmail.com>
2022-07-13 11:11:34 +0900

    add my text

--- game.py
+++ game.py
@@ -4,23 +4,23 @@ num1 = int(input("하나를 선택하세요 : 가위(0), 바위(1), 보(2) :"))
 num2 = random.randrange(0, 3)
 
 if num2 == 0:
-    print("컴퓨터는 가위를 냈습니다.")
+    print("컴퓨터는 가위를.")
 elif num2 == 1:
-    print("컴퓨터는 바위를 냈습니다.")
+    print("컴퓨터는 바위를.")
 else:
-    print("컴퓨터는 보를 냈습니다.")
+    print("컴퓨터는 보를.")
 
 
 if num1 == 0:
     if num2 == 1:
-        print("컴퓨터가 이겼습니다")
+        print("컴퓨터 승리")
     elif num2 == 2:
-        print("당신이 이겼습니다.")
+        print("플레이어 승리")
     else:
         print("비겼습니다.")
 elif num1 == 1:
     if num2 == 0:
-        print("당신이 이겼습니다.")
+        print("플레이어 승리.")
     elif num2 == 2:
         print("컴퓨터가 이겼습니다.")
     else:
